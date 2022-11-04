31e139dd8e8103ef0ca7ff75ec8b59904f408b8a
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2022-11-04 07:54:20 -0300

    Configura poetry para criar venvs no projeto

--- script-instalacao-programas-winget.ps1
+++ script-instalacao-programas-winget.ps1
@@ -4,5 +4,6 @@ nvs add lts
 nvs link lts
 npm install -g yarn
 python -m pip install -U poetry numpy matplotlib scipy pandas openpyxl jupyterlab
+poetry config virtualenvs.in-project true
 New-Item $env:HOMEPATH\.config\starship.toml
 Set-Content $env:HOMEPATH\.config\starship.toml "[package]`r`ndisabled=true`r`n`r`n[nodejs]`r`nsymbol='nodejs '"
