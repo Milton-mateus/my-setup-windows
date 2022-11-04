e1f09a05da97594b8eb183a84b90ffd28250d124
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2022-11-04 07:51:54 -0300

    Adiciona configuração do starship

--- script-instalacao-programas-winget.ps1
+++ script-instalacao-programas-winget.ps1
@@ -4,3 +4,5 @@ nvs add lts
 nvs link lts
 npm install -g yarn
 python -m pip install -U poetry numpy matplotlib scipy pandas openpyxl jupyterlab
+New-Item $env:HOMEPATH\.config\starship.toml
+Set-Content $env:HOMEPATH\.config\starship.toml "[package]`r`ndisabled=true`r`n`r`n[nodejs]`r`nsymbol='nodejs '"
