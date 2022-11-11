31baecb5930611bc1222680bcc1b24cf40f861dc
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2022-11-11 07:54:38 -0300

    Adiciona pdm e seaborn aos pacotes python

--- script-instalacao-programas-winget.ps1
+++ script-instalacao-programas-winget.ps1
@@ -3,7 +3,7 @@ winget install -h --id=Microsoft.VisualStudioCode -e  ; winget install -h --id=G
 nvs add lts
 nvs link lts
 npm install -g yarn
-python -m pip install -U poetry numpy matplotlib scipy pandas openpyxl jupyterlab
+python -m pip install -U pdm poetry numpy matplotlib scipy pandas seaborn openpyxl jupyterlab
 poetry config virtualenvs.in-project true
 New-Item $env:HOMEPATH\.config\starship.toml
 Set-Content $env:HOMEPATH\.config\starship.toml "[package]`r`ndisabled=true`r`n`r`n[nodejs]`r`nsymbol='nodejs '"
