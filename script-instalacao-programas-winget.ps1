c06375827c6d7727ae8abb32d3170a893473c4f1
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2022-12-02 07:46:59 -0300

    Remove script antigo

--- script-instalacao-programas-winget.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy Unrestricted
-winget install -h --id=Microsoft.VisualStudioCode -e  ; winget install -h --id=Git.Git -e  ; winget install -h --id=jasongin.nvs -e  ; winget install -h --id=Python.Python.3.10 -e  ; winget install -h --id=Microsoft.WindowsTerminal -e  ; winget install -h --id=Microsoft.PowerToys -e  ; winget install -h --id=Flameshot.Flameshot -e  ; winget install -h --id=RARLab.WinRAR -e  ; winget install -h --id=VideoLAN.VLC -e   ; winget install -h --id=ShareX.ShareX -e  ; winget install -h --id=SumatraPDF.SumatraPDF -e  ; winget install -h --id=Foxit.FoxitReader -e  ; winget install -h --id=Google.Chrome -e  ; winget install -h --id=Starship.Starship -e  ; winget install -h --id=Microsoft.PowerShell -e 
-nvs add lts
-nvs link lts
-npm install -g yarn
-python -m pip install -U pdm poetry numpy matplotlib scipy pandas seaborn openpyxl jupyterlab
-poetry config virtualenvs.in-project true
-New-Item $env:HOMEPATH\.config\starship.toml
-Set-Content $env:HOMEPATH\.config\starship.toml "[package]`r`ndisabled=true`r`n`r`n[nodejs]`r`nsymbol='nodejs '"
