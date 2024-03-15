3ec7085e63bb5d4d5d8cf64a82a3da92b54f1294
Matheus Amancio <56482846+matheus-amancio@users.noreply.github.com>
2024-03-15 10:43:08 -0300

    Update programming.ps1

--- programming.ps1
+++ programming.ps1
@@ -1,7 +1,7 @@
 nvs add lts
 nvs link lts
 npm install -g yarn
-python -m pip install -U pdm poetry numpy matplotlib scipy pandas seaborn openpyxl jupyterlab hvplot plotly requests
+python -m pip install -U poetry numpy matplotlib scipy pandas pyarrow seaborn openpyxl jupyterlab hvplot plotly requests
 poetry config virtualenvs.in-project true
 New-Item $env:HOMEPATH\.config\starship.toml
 Set-Content $env:HOMEPATH\.config\starship.toml "[package]`r`ndisabled=true`r`n`r`n[nodejs]`r`nsymbol='nodejs '"
